3eea09bd995c717c48cb28b0e8923bdd73fab143
Guilherme Ovalhe <guiovalhe@gmail.com>
2025-01-08 02:06:41 -0300

    commit finalizando modulo 1

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises'
 
 const databasePath = new URL('../db.json', import.meta.url)
-console.log(databasePath)
+
 export class Database {
     #database = {}
 
@@ -9,17 +9,28 @@ export class Database {
         fs.readFile(databasePath, 'utf-8').then(data => {
             this.#database = JSON.parse(data)
         })
-        .catch(() =>{
-            this.#persist()
-        })
+            .catch(() => {
+                this.#persist()
+            })
     }
 
     #persist() {
         fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
-    select(table) {
-        const data = this.#database[table] ?? []
+    // { name: "Diego", email: "Diego" }
+    // [ ['name', 'Diego'], ['email', 'Diego'] ]
+
+    select(table, search) {
+        let data = this.#database[table] ?? []
+
+        if (search) {
+            data = data.filter(row => {
+                return Object.entries(search).some(([key, value]) => {
+                    return row[key].toLowerCase().includes(value.toLowerCase())
+                })
+            })
+        }
 
         return data
     }
@@ -32,5 +43,24 @@ export class Database {
         }
 
         this.#persist();
+        return data;
+    }
+
+    update(table, id, data) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table][rowIndex] = { id, ...data }
+            this.#persist()
+        }
+    }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
     }
 }
